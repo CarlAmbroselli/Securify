074f9f2476f42ce451d7ba331853b2d108590f28
Carl Ambroselli <git@carl-ambroselli.de>
2016-05-03 23:53:59 +0200

    Update texts

--- js/main.js
+++ js/main.js
@@ -12,11 +12,11 @@ jQuery(document).ready(function($) {
   $('#main-nav .nav').onePageNav({
     currentClass: 'active',
     scrollOffset: 69,
-  });  
+  });
 });
 
 $(document).ready(function(){
-   
+
   //.parallax(xPosition, speedFactor, outerHeight) options:
   //xPosition - Horizontal position of the element
   //inertia - speed to move relative to vertical scroll. Example: 0.1 is one tenth the speed of scrolling, 2 is twice the speed of scrolling
@@ -26,23 +26,23 @@ $(document).ready(function(){
   $('#download').parallax("50%", 0.4);
 })
 
-
-$(document).ready(function() {
-      $(".owl-carousel").owlCarousel({
-        autoPlay: 3000,
-        items : 4,
-        itemsDesktop : [1199,3],
-        itemsDesktopSmall : [979,3]
-      });
-
-    });
-
-    jQuery(function( $ ){
-          $('#download-app1').localScroll({
-            duration:1200
-          });
-           $('#download-app2').localScroll({
-            duration:1000
-          });
-        });
-
+//
+// $(document).ready(function() {
+//       $(".owl-carousel").owlCarousel({
+//         autoPlay: 3000,
+//         items : 4,
+//         itemsDesktop : [1199,3],
+//         itemsDesktopSmall : [979,3]
+//       });
+//
+//     });
+//
+//     jQuery(function( $ ){
+//           $('#download-app1').localScroll({
+//             duration:1200
+//           });
+//            $('#download-app2').localScroll({
+//             duration:1000
+//           });
+//         });
+//
